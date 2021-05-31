d33ee344cb4b4dfa4e58c3db4f6c6180ec475a53
LWJerri <CLWJerri@yandex.ua>
2021-05-31 22:37:24 +0300

    Add links module

---
+++ src/modules/links.svelte
@@ -0,0 +1,28 @@
+<div class="links">
+    <h1>LINKS</h1>
+    <a href="https://vk.com/LWJerri"><i class="fab fa-vk fa-5x" /></a>
+    <a href="https://t.me/LWJerri"><i class="fab fa-telegram-plane fa-5x" /></a>
+    <a href="https://discords.com/bio/p/432085389948485633"
+        ><i class="fab fa-discord fa-5x" /></a
+    >
+    <a href="https://github.com/LWJerri"><i class="fab fa-github fa-5x" /></a>
+</div>
+
+<style>
+    i:hover {
+        opacity: 0.7;
+    }
+
+    h1 {
+        color: #fff;
+    }
+
+    i {
+        color: #fff;
+        margin: 0px 5px 0px 5px;
+    }
+
+    .links {
+        margin: 25px 0px 50px 0px;
+    }
+</style>
