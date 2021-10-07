0dcedc5ee6a3a52d61b13632f18cd42bfe904263
LWJerri <CLWJerri@yandex.ua>
2021-10-07 19:16:46 +0300

    Finish Main.svelte and APp.svlete files

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Router, Route } from "svelte-navigator";
+  import Main from "./pages/Main.svelte";
+</script>
+
+<Router>
+  <main>
+    <Route component={Main} />
+  </main>
+</Router>
