c645a016e8cf05b569279041f24e5e17ba21ee80
LWJerri <CLWJerri@yandex.ua>
2021-10-07 21:36:57 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,10 @@ cd dist
 # if you are deploying to a custom domain
 echo "lwjerri.js.org" > CNAME
 
-git init
-git add -A
-git commit -m "Upload new website version"
+cd -
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:LWJerri/LWJerri.github.io.git gh-pages
+git add dist
+git commit -m "Upload new website version"
+git subtree push --prefix dist origin gh-pages
 
 cd -
