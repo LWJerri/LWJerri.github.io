c40c261fff7d432fa12f24c3d6170259b67cdb72
LWJerri <CLWJerri@yandex.ua>
2021-12-25 22:57:50 +0200

    123

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,23 @@
 # Abort on errors
 set -e
 
-# Install & Build
-yarn
-yarn build
 
 git init
 
 git config --global user.email "CLWJerri@yandex.ua"
 git config --global user.name "LWJerri"
-git push origin gh-pages:develop --force
+
+# Install & Build
+yarn
+yarn build
+
+cd dist
+
+git checkout develop
+git add -A
+git commit -m "Deploy new code"
+
+git push origin workflows_build:develop --force
 # Move to dist folder
 #cd dist
 
