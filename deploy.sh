40609f7ea5d5a8b321005970a52dc3e841b9035a
LWJerri <CLWJerri@yandex.ua>
2021-12-25 16:07:33 +0200

    234

--- deploy.sh
+++ deploy.sh
@@ -21,4 +21,4 @@ git config --global user.name "LWJerri"
 git add -A
 git commit -m "Deploy new code"
 cd -
-git push -f git@github.com:LWJerri/lwjerri.github.io.git origin:gh-pages
+git push -f gh-pages
