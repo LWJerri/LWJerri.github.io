673d08ec044f8d181cf315c30d4787d5f0256803
LWJerri <CLWJerri@yandex.ua>
2021-12-25 14:13:31 +0200

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# Abort on errors
+set -e
+
+# Install & Build
+yarn
+yarn build
+
+# Move to dist folder
+cd dist
+
+# Add CNMAE file
+echo "lwjerri.js.org" > CNAME
+
+git init
+
+git config --global user.email $EMAIL
+git config --global user.name $USERNAME
+
+git add -A
+git commit -m "Deplot new code"
+
+git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git gh-pages
+
+cd -
