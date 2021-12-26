c305a6e5da831bac539ab8965321e88318a20898
LWJerri <CLWJerri@yandex.ua>
2021-12-26 17:20:20 +0200

    dg

--- deploy.sh
+++ deploy.sh
@@ -21,10 +21,10 @@ git config --global user.name "LWJerri"
 git add -A
 git commit -m "Deploy code changes"
 
-#git push -f https://$1@github.com/LWJerri/lwjerri.github.io.git develop
+git push -f https://$1@github.com/LWJerri/lwjerri.github.io.git
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:LWJerri/LWJerri.github.io.git develop
+#git push -f git@github.com:LWJerri/LWJerri.github.io.git develop
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
