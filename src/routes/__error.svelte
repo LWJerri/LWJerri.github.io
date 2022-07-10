9c84df5cc9c76eceee83c08ea5398c402a52c5d8
LWJerri <loginmanx@gmail.com>
2022-07-10 10:34:52 +0000

    Upload basic config files

---
+++ src/routes/__error.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+  export function load({ error, status }) {
+    return {
+      props: {
+        title: `${status}: ${error.message}`,
+      },
+    };
+  }
+</script>
+
+<script>
+  export let title;
+</script>
+
+<h1>{title}</h1>
---
+++ src/routes/api/index.ts
@@ -0,0 +1,11 @@
+export async function get() {
+  return {
+    status: 200,
+    headers: {
+      "access-control-allow-origin": "*",
+    },
+    body: {
+      number: Math.random(),
+    },
+  };
+}
