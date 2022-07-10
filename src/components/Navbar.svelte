80c45fe5330e1a9a30f44ae240ca94053ce0420b
LWJerri <loginmanx@gmail.com>
2022-07-10 10:49:05 +0000

    upd

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Button, Navbar, NavBrand, NavHamburger, NavLi, NavUl } from "flowbite-svelte";
+</script>
+
+<Navbar let:hidden let:toggle>
+  <NavBrand href="/">
+    <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"> Flowbite </span>
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+  <NavUl {hidden}>
+    <NavLi href="/" active={true}><Button>Default</Button></NavLi>
+  </NavUl>
+</Navbar>
