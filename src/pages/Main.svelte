ce18c1e3cfb5de62e23fa3fcb39c290846083fa8
LWJerri <loginmanx@gmail.com>
2023-01-13 03:17:13 +0200

    Latest v2.1.0 release for this website

--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -1,154 +1,41 @@
 <script lang="ts">
-  import logo from "../assets/logo.png";
-  import data from "../assets/data.json";
+  import { onMount } from "svelte";
+  const newURL = "https://lwjerri.dev";
+
+  onMount(() => {
+    setTimeout(() => {
+      window.location.href = newURL;
+    }, 5000);
+  });
 </script>
 
-<svelte:head>
-  <title>Andrey Zontov &#149; Main</title>
-</svelte:head>
-
 <div class="main">
-  <div class="box">
-    <img src={logo} alt="AVATAR" />
-    <h1 id="name">Andrey Zontov</h1>
-    <p id="about">Backend Node.js developer</p>
-  </div>
+  <div style="text-align: center;">
+    <img src="https://i.imgur.com/MlJSRqn.png" alt="AVATAR" style="width: 20vh;border-radius:100%;" />
+    <h1 id="name" style="outline: none;font-size: 6vh;">Andrey Zontov</h1>
+    <p style="font-size: 3.5vh;">
+      Hello! This is my old personal page.<br />Use new link or you will be redirected after 5 seconds!
+    </p>
 
-  <div class="box">
-    <h1 class="title">Contacts</h1>
-
-    {#each data.contacts as contact}
-      <a id="link" href={contact.link} target="_blank"><i class={contact.icon} /></a>
-    {/each}
-  </div>
-
-  <div class="box">
-    <h1 class="title">Projects</h1>
-
-    {#each data.projects as project}
-      <div class="project">
-        <h1>
-          {project.name}
-        </h1>
-
-        {#each project.links as link}
-          {@html `<a href=${link} target="_blank" style="color: #22B8CF; font-size: 1.875rem; margin: 0px 5px 0px 5px"><i class="fas fa-link"></i></a>`}
-        {/each}
-
-        <article>
-          <p>{project.description}</p>
-          <h3><i>{project.tools.join(", ")}</i></h3>
-        </article>
-      </div>
-    {/each}
+    <p style="font-size: 3.5vh;">
+      <a id="url" href={newURL}>{newURL}</a> <span>[<b>Clickable</b>]</span>
+    </p>
   </div>
 </div>
 
 <style>
-  * {
-    color: white;
-    text-align: center;
-    word-break: normal;
-  }
-
   .main {
-    margin: 1.875rem 9.375rem 1.875rem 9.375rem;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
-  @media screen and (max-width: 64rem) {
-    .main {
-      margin: 1.875rem 5rem 1.875rem 5rem;
-    }
+  #url {
+    text-decoration: none;
   }
 
-  @media screen and (max-width: 48rem) {
-    .main {
-      margin: 1.875rem 1.25rem 1.875rem 1.25rem;
-    }
-  }
-
-  @media screen and (max-width: 26.563rem) {
-    .main {
-      margin: 1.875rem 0.313rem 1.875rem 0.313rem;
-    }
-
-    .box img {
-      display: none;
-    }
-  }
-
-  .box {
-    margin: 1.875rem 0rem 1.875rem 0rem;
-  }
-
-  .box img {
-    -webkit-user-select: none;
-    -webkit-user-drag: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    width: 16rem;
-    height: 16rem;
-  }
-
-  .box h1 {
-    font-size: 2.813rem;
-  }
-
-  .box #name {
-    -webkit-user-select: none;
-    -webkit-user-drag: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    font-size: 3.125rem;
-    outline: none;
-  }
-
-  .box #about {
-    -webkit-user-select: none;
-    -webkit-user-drag: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    font-size: 2.188rem;
-  }
-
-  .box #link {
-    margin: 0rem 0.313rem 0rem 0.313rem;
-  }
-
-  .box #link:hover {
-    opacity: 0.7;
-  }
-
-  .project {
-    margin-top: 2.188rem;
-  }
-
-  .project h1 {
-    color: #22b8cf;
-  }
-
-  .project article h3 {
-    font-size: 1.25rem;
-    margin-top: 0.625rem;
-  }
-
-  .project article p {
-    font-size: 1.75rem;
-  }
-
-  .title {
-    -webkit-user-select: none;
-    -webkit-user-drag: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    margin-bottom: 0.938rem;
+  #url:hover {
+    text-decoration: underline;
   }
 </style>
